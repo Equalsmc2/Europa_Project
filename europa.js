0b2747550baa78cbf3d7c9ccf9dedbb87d1fd774
Equalsmc2 <155104649+Equalsmc2@users.noreply.github.com>
2026-08-14 13:34:21 -0400

    Reduce planet radius scaling factor to 0.5

--- europa.js
+++ europa.js
@@ -120,7 +120,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const sphere = box.getBoundingSphere(new THREE.Sphere());
       
       const planetRadius = sphere.radius;
-      const R = planetRadius * 1.0; 
+      const R = planetRadius * 0.5; 
       
       planetGroup.add(planetModel);
 
