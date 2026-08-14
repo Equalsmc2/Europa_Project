25b53c17fd3a371384aefb79f2e03dda7e0e78d1
Equalsmc2 <155104649+Equalsmc2@users.noreply.github.com>
2026-08-14 13:44:11 -0400

    Update europa.js

--- europa.js
+++ europa.js
@@ -120,7 +120,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const sphere = box.getBoundingSphere(new THREE.Sphere());
       
       const planetRadius = sphere.radius;
-      const R = planetRadius * 0.75; 
+      const R = planetRadius * 0.55; 
       
       planetGroup.add(planetModel);
 
