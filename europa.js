3b28c3f09d1ca7e72ed6e98001c99eb32ae15cab
Equalsmc2 <155104649+Equalsmc2@users.noreply.github.com>
2026-08-14 02:09:47 -0400

    Update Firebase API key in europa.js

--- europa.js
+++ europa.js
@@ -324,7 +324,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // FIREBASE & TERMINAL LOGIC
   // ==========================================
   const config = {
-    apiKey: "YOUR_API_KEY", 
+    apiKey: "AIzaSyB2nuuvLSrXQiHPRSWq-TwcTKEQ_Zedbz0",
     projectId: "europa-4b0d3" 
   };
   
